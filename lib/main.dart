c0b1af3d36b080d91e853db1e3c9bc70ebec28e0
SatisKia <satis@msd.biglobe.ne.jp>
2022-02-20 15:00:58 +0900

    simple_animations

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:simple_animations/simple_animations.dart';
 
 import 'test.dart';
 
@@ -21,69 +22,157 @@ class MyHomePage extends StatefulWidget {
   const MyHomePage({Key? key}) : super(key: key);
 
   @override
-  State createState() => _MyHomePageState();
+  State createState() => MyHomePageState();
 }
 
-class _MyHomePageState extends State {
+class MyHomePageState extends State {
   double contentWidth  = 0.0;
   double contentHeight = 0.0;
+
+  ScrollController? hogeScrollController;
+  double hogeScroll = 0.0; // 現在のスクロール位置
+  double maxHogeScroll = 0.0; // スクロールできる最大の位置
+  void onHogeScroll(){
+    hogeScroll = hogeScrollController!.offset;
+    maxHogeScroll =
+        hogeScrollController!.position.maxScrollExtent;
+
+    if( hogeScroll < contentHeight / 5 ){
+      // フローティングアクションボタンを隠す
+      if( !hide ) {
+        hide = true;
+        setState(() {
+          hogeAnimationControl = CustomAnimationControl.playFromStart;
+        });
+      }
+    } else {
+      // フローティングアクションボタンを表示させる
+      if( hide ) {
+        hide = false;
+        setState(() {
+          hogeAnimationControl = CustomAnimationControl.playFromStart;
+        });
+      }
+    }
+  }
+
+  bool hide = true;
+  CustomAnimationControl? hogeAnimationControl;
+
+  void scrollTop(){
+    setState(() {
+      hogeScrollController!.jumpTo(0.0);
+    });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    hogeScrollController = ScrollController();
+    hogeScrollController!.addListener( onHogeScroll );
+  }
+
+  @override
+  void dispose() {
+    hogeScrollController!.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
     contentWidth  = MediaQuery.of( context ).size.width;
     contentHeight = MediaQuery.of( context ).size.height - MediaQuery.of( context ).padding.top - MediaQuery.of( context ).padding.bottom;
 
-    Widget body = Container(
-      width: contentWidth,
-      height: contentHeight,
-      color: Colors.black12,
-    );
+    bool scroll = false;
+    if( !scroll ){
 
-    bool positioned = false;
+      Widget body = Container(
+        width: contentWidth,
+        height: contentHeight,
+        color: Colors.black12,
+      );
 
-    if( !positioned ) {
-      // フローティングアクションボタンの位置
-      FloatingActionButtonLocation floatingLocation = FloatingActionButtonLocation.endFloat;
-      bool mini = false;
-      switch( floatingLocation ){
-        case FloatingActionButtonLocation.miniCenterDocked:
-        case FloatingActionButtonLocation.miniCenterFloat:
-        case FloatingActionButtonLocation.miniCenterTop:
-        case FloatingActionButtonLocation.miniEndDocked:
-        case FloatingActionButtonLocation.miniEndFloat:
-        case FloatingActionButtonLocation.miniEndTop:
-        case FloatingActionButtonLocation.miniStartDocked:
-        case FloatingActionButtonLocation.miniStartFloat:
-        case FloatingActionButtonLocation.miniStartTop:
-          mini = true;
+      bool positioned = true;
+      if( !positioned ) {
+
+        // フローティングアクションボタンの位置
+        FloatingActionButtonLocation floatingLocation = FloatingActionButtonLocation.endFloat;
+        bool mini = false;
+        switch( floatingLocation ){
+          case FloatingActionButtonLocation.miniCenterDocked:
+          case FloatingActionButtonLocation.miniCenterFloat:
+          case FloatingActionButtonLocation.miniCenterTop:
+          case FloatingActionButtonLocation.miniEndDocked:
+          case FloatingActionButtonLocation.miniEndFloat:
+          case FloatingActionButtonLocation.miniEndTop:
+          case FloatingActionButtonLocation.miniStartDocked:
+          case FloatingActionButtonLocation.miniStartFloat:
+          case FloatingActionButtonLocation.miniStartTop:
+            mini = true;
+        }
+
+        FloatingActionButton floating = test1( mini );
+//        FloatingActionButton floating = test2();
+
+        return Scaffold(
+          appBar: AppBar(
+              toolbarHeight: 0
+          ),
+          body: body,
+          floatingActionButton: floating,
+          floatingActionButtonLocation: floatingLocation,
+        );
+
+      } else {
+
+//        Widget floating = test3();
+//        Widget floating = test4();
+//        Widget floating = test5();
+//        Widget floating = test6();
+//        Widget floating = test7(false);
+        Widget floating = test7(true);
+
+        return Scaffold(
+          appBar: AppBar(
+              toolbarHeight: 0
+          ),
+          body: Stack( children: [
+            body,
+            floating,
+          ] ),
+        );
       }
 
-      FloatingActionButton floating = test1( mini );
-//      FloatingActionButton floating = test2();
-
-      return Scaffold(
-        appBar: AppBar(
-            toolbarHeight: 0
-        ),
-        body: body,
-        floatingActionButton: floating,
-        floatingActionButtonLocation: floatingLocation,
-      );
     } else {
-      Widget floating = test3();
-//      Widget floating = test4();
-//      Widget floating = test5();
-//      Widget floating = test6();
+
+      List<Widget> list = [];
+      for( int i = 0; i < 20; i++ ){
+        list.add( Container(
+            width: contentWidth,
+            height: contentHeight / 5,
+            color: ((i % 2) == 0) ? Color(0xFFFFFFFF) : Color(0xFFEEFFFF)
+        ));
+      }
+
+      SingleChildScrollView scrollView = SingleChildScrollView(
+        controller: hogeScrollController,
+        scrollDirection: Axis.vertical,
+        child: Column( children: list ),
+      );
+
+      Widget floating = test8( this, contentWidth, contentHeight, hide, hogeAnimationControl, false );
+//      Widget floating = test8( this, contentWidth, contentHeight, hide, hogeAnimationControl, true );
 
       return Scaffold(
         appBar: AppBar(
             toolbarHeight: 0
         ),
         body: Stack( children: [
-          body,
+          scrollView,
           floating,
         ] ),
       );
+
     }
   }
 }
--- lib/test.dart
+++ lib/test.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:simple_animations/simple_animations.dart';
+
+import 'main.dart';
 
 FloatingActionButton test1( bool mini ) {
   return FloatingActionButton(
@@ -45,6 +48,58 @@ Positioned test4() {
 }
 
 Positioned test5() {
+  Widget button1 = ElevatedButton(
+    child: const Icon(
+        Icons.flutter_dash,
+        color: Colors.white
+    ),
+    style: ButtonStyle(
+      padding: MaterialStateProperty.all(EdgeInsets.zero),
+      shape: MaterialStateProperty.all(const CircleBorder(
+        side: BorderSide.none,
+      )),
+      backgroundColor: MaterialStateProperty.all(Colors.blue),
+      elevation: MaterialStateProperty.all(8.0),
+    ),
+    onPressed: () {
+      debugPrint('onPressed blue');
+    },
+  );
+  Widget button2 = ElevatedButton(
+    child: const Icon(
+        Icons.flutter_dash,
+        color: Colors.white
+    ),
+    style: ButtonStyle(
+      padding: MaterialStateProperty.all(EdgeInsets.zero),
+      shape: MaterialStateProperty.all(const CircleBorder(
+        side: BorderSide.none,
+      )),
+      backgroundColor: MaterialStateProperty.all(Colors.amber),
+      elevation: MaterialStateProperty.all(8.0),
+    ),
+    onPressed: () {
+      debugPrint('onPressed amber');
+    },
+  );
+  Widget button3 = ElevatedButton(
+    child: const Icon(
+        Icons.flutter_dash,
+        color: Colors.white
+    ),
+    style: ButtonStyle(
+      padding: MaterialStateProperty.all(EdgeInsets.zero),
+      shape: MaterialStateProperty.all(const CircleBorder(
+        side: BorderSide.none,
+      )),
+      backgroundColor: MaterialStateProperty.all(Colors.pink),
+      elevation: MaterialStateProperty.all(8.0),
+    ),
+    onPressed: () {
+      debugPrint('onPressed pink');
+    },
+  );
+
   return Positioned(
     left: 10,
     top: 10,
@@ -52,23 +107,7 @@ Positioned test5() {
       SizedBox(
         width: 50,
         height: 50,
-        child: ElevatedButton(
-          child: const Icon(
-              Icons.flutter_dash,
-              color: Colors.white
-          ),
-          style: ButtonStyle(
-            padding: MaterialStateProperty.all(EdgeInsets.zero),
-            shape: MaterialStateProperty.all(const CircleBorder(
-              side: BorderSide.none,
-            )),
-            backgroundColor: MaterialStateProperty.all(Colors.blue),
-            elevation: MaterialStateProperty.all(8.0),
-          ),
-          onPressed: () {
-            debugPrint('onPressed blue');
-          },
-        ),
+        child: button1,
       ),
       SizedBox(
           height: 10
@@ -76,23 +115,7 @@ Positioned test5() {
       SizedBox(
         width: 50,
         height: 50,
-        child: ElevatedButton(
-          child: const Icon(
-              Icons.flutter_dash,
-              color: Colors.white
-          ),
-          style: ButtonStyle(
-            padding: MaterialStateProperty.all(EdgeInsets.zero),
-            shape: MaterialStateProperty.all(const CircleBorder(
-              side: BorderSide.none,
-            )),
-            backgroundColor: MaterialStateProperty.all(Colors.amber),
-            elevation: MaterialStateProperty.all(8.0),
-          ),
-          onPressed: () {
-            debugPrint('onPressed amber');
-          },
-        ),
+        child: button2,
       ),
       SizedBox(
           height: 10
@@ -100,23 +123,7 @@ Positioned test5() {
       SizedBox(
         width: 50,
         height: 50,
-        child: ElevatedButton(
-          child: const Icon(
-              Icons.flutter_dash,
-              color: Colors.white
-          ),
-          style: ButtonStyle(
-            padding: MaterialStateProperty.all(EdgeInsets.zero),
-            shape: MaterialStateProperty.all(const CircleBorder(
-              side: BorderSide.none,
-            )),
-            backgroundColor: MaterialStateProperty.all(Colors.pink),
-            elevation: MaterialStateProperty.all(8.0),
-          ),
-          onPressed: () {
-            debugPrint('onPressed pink');
-          },
-        ),
+        child: button3,
       ),
     ] ),
   );
@@ -174,4 +181,305 @@ Positioned test6() {
       ] ),
     ),
   );
+}
+
+Widget slide({required Widget child, required Duration delay, required Tween<double> tween, required Curve curve, required Duration duration}) {
+  return PlayAnimation<double>(
+    delay: delay,
+    tween: tween,
+    curve: curve,
+    duration: duration,
+    child: child,
+    builder: (context, child, value) => Transform.translate(
+        offset: Offset(value, 0.0),
+        child: child
+    ),
+  );
+}
+
+Widget slideWithOpacity({required Widget child, required Duration delay, required Tween<double> translate, required Curve curve, required Tween<double> opacity, required Duration duration}) {
+  MultiTween<String> tween = MultiTween<String>();
+  tween.add('translate', translate, duration, curve);
+  tween.add('opacity', opacity, duration);
+
+  return PlayAnimation<MultiTweenValues<String>>(
+    delay: delay,
+    tween: tween,
+    duration: tween.duration,
+    child: child,
+    builder: (context, child, value) => Transform.translate(
+      offset: Offset(value.get('translate'), 0.0),
+      child: Opacity(
+          opacity: value.get('opacity'),
+          child: child
+      ),
+    ),
+  );
+}
+
+Positioned test7(bool opacity) {
+  Widget button1 = ElevatedButton(
+    child: const Icon(
+        Icons.flutter_dash,
+        color: Colors.white
+    ),
+    style: ButtonStyle(
+      padding: MaterialStateProperty.all(EdgeInsets.zero),
+      shape: MaterialStateProperty.all(const CircleBorder(
+        side: BorderSide.none,
+      )),
+      backgroundColor: MaterialStateProperty.all(Colors.blue),
+      elevation: MaterialStateProperty.all(8.0),
+    ),
+    onPressed: () {
+      debugPrint('onPressed blue');
+    },
+  );
+  Widget button2 = ElevatedButton(
+    child: const Icon(
+        Icons.flutter_dash,
+        color: Colors.white
+    ),
+    style: ButtonStyle(
+      padding: MaterialStateProperty.all(EdgeInsets.zero),
+      shape: MaterialStateProperty.all(const CircleBorder(
+        side: BorderSide.none,
+      )),
+      backgroundColor: MaterialStateProperty.all(Colors.amber),
+      elevation: MaterialStateProperty.all(8.0),
+    ),
+    onPressed: () {
+      debugPrint('onPressed amber');
+    },
+  );
+  Widget button3 = ElevatedButton(
+    child: const Icon(
+        Icons.flutter_dash,
+        color: Colors.white
+    ),
+    style: ButtonStyle(
+      padding: MaterialStateProperty.all(EdgeInsets.zero),
+      shape: MaterialStateProperty.all(const CircleBorder(
+        side: BorderSide.none,
+      )),
+      backgroundColor: MaterialStateProperty.all(Colors.pink),
+      elevation: MaterialStateProperty.all(8.0),
+    ),
+    onPressed: () {
+      debugPrint('onPressed pink');
+    },
+  );
+
+  if( !opacity ){
+    return Positioned(
+      left: 10,
+      top: 10,
+      child: Column( children: [
+        SizedBox(
+          width: 50,
+          height: 50,
+          child: slide(
+            delay: Duration.zero,
+            tween: Tween(begin: -70.0, end: 0.0),
+            curve: Curves.easeOut,
+            duration: Duration(milliseconds: 500),
+            child: button1,
+          ),
+        ),
+        SizedBox(
+            height: 10
+        ),
+        SizedBox(
+          width: 50,
+          height: 50,
+          child: slide(
+            delay: Duration(milliseconds: 300),
+            tween: Tween(begin: -70.0, end: 0.0),
+            curve: Curves.easeOut,
+            duration: Duration(milliseconds: 500),
+            child: button2,
+          ),
+        ),
+        SizedBox(
+            height: 10
+        ),
+        SizedBox(
+          width: 50,
+          height: 50,
+          child: slide(
+            delay: Duration(milliseconds: 600),
+            tween: Tween(begin: -70.0, end: 0.0),
+            curve: Curves.easeOut,
+            duration: Duration(milliseconds: 500),
+            child: button3,
+          ),
+        ),
+      ] ),
+    );
+  } else {
+    return Positioned(
+      left: 10,
+      top: 10,
+      child: Column( children: [
+        SizedBox(
+          width: 50,
+          height: 50,
+          child: slideWithOpacity(
+            delay: Duration.zero,
+            translate: Tween(begin: -70.0, end: 0.0),
+            curve: Curves.easeOut,
+            opacity: Tween(begin: 0.0, end: 1.0),
+            duration: Duration(milliseconds: 500),
+            child: button1,
+          ),
+        ),
+        SizedBox(
+            height: 10
+        ),
+        SizedBox(
+          width: 50,
+          height: 50,
+          child: slideWithOpacity(
+            delay: Duration(milliseconds: 300),
+            translate: Tween(begin: -70.0, end: 0.0),
+            curve: Curves.easeOut,
+            opacity: Tween(begin: 0.0, end: 1.0),
+            duration: Duration(milliseconds: 500),
+            child: button2,
+          ),
+        ),
+        SizedBox(
+            height: 10
+        ),
+        SizedBox(
+          width: 50,
+          height: 50,
+          child: slideWithOpacity(
+            delay: Duration(milliseconds: 600),
+            translate: Tween(begin: -70.0, end: 0.0),
+            curve: Curves.easeOut,
+            opacity: Tween(begin: 0.0, end: 1.0),
+            duration: Duration(milliseconds: 500),
+            child: button3,
+          ),
+        ),
+      ] ),
+    );
+  }
+}
+
+Widget slideControl(CustomAnimationControl control, {required Widget child, required Tween<double> tween, required Curve curve, required Duration duration}) {
+  return CustomAnimation<double>(
+    control: control,
+    tween: tween,
+    curve: curve,
+    duration: duration,
+    child: child,
+    builder: (context, child, value) => Transform.translate(
+        offset: Offset(value, 0.0),
+        child: child
+    ),
+  );
+}
+
+Widget slideControlWithOpacity(CustomAnimationControl control, {required Widget child, required Tween<double> translate, required Curve curve, required Tween<double> opacity, required Duration duration}) {
+  MultiTween<String> tween = MultiTween<String>();
+  tween.add('translate', translate, duration, curve);
+  tween.add('opacity', opacity, duration);
+
+  return CustomAnimation<MultiTweenValues<String>>(
+    control: control,
+    tween: tween,
+    duration: tween.duration,
+    child: child,
+    builder: (context, child, value) => Transform.translate(
+      offset: Offset(value.get('translate'), 0.0),
+      child: Opacity(
+          opacity: value.get('opacity'),
+          child: child
+      ),
+    ),
+  );
+}
+
+Widget test8( MyHomePageState state, double contentWidth, double contentHeight, bool hide, CustomAnimationControl? control, bool opacity ) {
+  Widget button = SizedBox(
+    width: 50,
+    height: 50,
+    child: ElevatedButton(
+      child: const Icon(
+          Icons.arrow_upward,
+          color: Colors.white
+      ),
+      style: ButtonStyle(
+        padding: MaterialStateProperty.all(EdgeInsets.zero),
+        shape: MaterialStateProperty.all(const CircleBorder(
+          side: BorderSide.none,
+        )),
+        backgroundColor: MaterialStateProperty.all(Colors.blue),
+        elevation: MaterialStateProperty.all(8.0),
+      ),
+      onPressed: () {
+        state.scrollTop();
+      },
+    ),
+  );
+
+  if( hide ){
+    // フローティングアクションボタンを隠す
+    if( !opacity ){
+      return Positioned(
+          left: contentWidth + 10,
+          top: contentHeight - 60,
+          child: (control == null) ? button : slideControl(
+            control,
+            tween: Tween(begin: -70.0, end: 0.0),
+            curve: Curves.easeOut,
+            duration: Duration(milliseconds: 500),
+            child: button,
+          )
+      );
+    } else {
+      return Positioned(
+          left: contentWidth + 10,
+          top: contentHeight - 60,
+          child: (control == null) ? button : slideControlWithOpacity(
+            control,
+            translate: Tween(begin: -70.0, end: 0.0),
+            curve: Curves.easeOut,
+            opacity: Tween(begin: 1.0, end: 0.0),
+            duration: Duration(milliseconds: 500),
+            child: button,
+          )
+      );
+    }
+  } else {
+    // フローティングアクションボタンを表示させる
+    if( !opacity ){
+      return Positioned(
+          left: contentWidth + 10,
+          top: contentHeight - 60,
+          child: (control == null) ? button : slideControl(
+            control,
+            tween: Tween(begin: 0.0, end: -70.0),
+            curve: Curves.easeOut,
+            duration: Duration(milliseconds: 500),
+            child: button,
+          )
+      );
+    } else {
+      return Positioned(
+          left: contentWidth + 10,
+          top: contentHeight - 60,
+          child: (control == null) ? button : slideControlWithOpacity(
+            control,
+            translate: Tween(begin: 0.0, end: -70.0),
+            curve: Curves.easeOut,
+            opacity: Tween(begin: 0.0, end: 1.0),
+            duration: Duration(milliseconds: 500),
+            child: button,
+          )
+      );
+    }
+  }
 }
